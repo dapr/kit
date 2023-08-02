90b1d0d906e06004102aeaf6ccd04c7a60b701d0
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-08-02 20:02:47 +0000

    Added eventqueue package
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

---
+++ eventqueue/eventqueue.go
@@ -0,0 +1,18 @@
+/*
+Copyright 2023 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package eventqueue implements a queue processor for delayed events.
+// Events are maintained in an in-memory queue, where items are in the order of when they are to be executed.
+// Users should interact with the Processor to process events in the queue.
+// When the queue has at least 1 item, the processor uses a single background goroutine to wait on the next item to be executed.
+package eventqueue
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/stretchr/testify v1.8.4
 	golang.org/x/crypto v0.10.0
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
+	k8s.io/utils v0.0.0-20230726121419-3b25d923346b
 )
 
 require (
