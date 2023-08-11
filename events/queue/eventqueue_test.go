8072d5a93717efecf1712437a2f7b5d4d1e11193
joshvanl <me@joshvanl.dev>
2023-08-11 16:36:58 +0100

    Don't pass clock to processor, and speed up tests
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- events/queue/eventqueue_test.go
+++ events/queue/eventqueue_test.go
@@ -16,8 +16,6 @@ package queue
 import (
 	"fmt"
 	"time"
-
-	kclock "k8s.io/utils/clock"
 )
 
 // queueableItem is an item that can be queued and it's used for testing.
@@ -38,9 +36,6 @@ func (r queueableItem) ScheduledTime() time.Time {
 }
 
 func ExampleProcessor() {
-	// Init a clock using k8s.io/utils/clock
-	clock := kclock.RealClock{}
-
 	// Method invoked when an item is to be executed
 	executed := make(chan string, 3)
 	executeFn := func(r *queueableItem) {
@@ -48,16 +43,16 @@ func ExampleProcessor() {
 	}
 
 	// Create the processor
-	processor := NewProcessor[*queueableItem](executeFn, clock)
+	processor := NewProcessor[*queueableItem](executeFn)
 
 	// Add items to the processor, in any order, using Enqueue
-	processor.Enqueue(&queueableItem{Name: "item1", ExecutionTime: clock.Now().Add(500 * time.Millisecond)})
-	processor.Enqueue(&queueableItem{Name: "item2", ExecutionTime: clock.Now().Add(200 * time.Millisecond)})
-	processor.Enqueue(&queueableItem{Name: "item3", ExecutionTime: clock.Now().Add(300 * time.Millisecond)})
-	processor.Enqueue(&queueableItem{Name: "item4", ExecutionTime: clock.Now().Add(time.Second)})
+	processor.Enqueue(&queueableItem{Name: "item1", ExecutionTime: time.Now().Add(500 * time.Millisecond)})
+	processor.Enqueue(&queueableItem{Name: "item2", ExecutionTime: time.Now().Add(200 * time.Millisecond)})
+	processor.Enqueue(&queueableItem{Name: "item3", ExecutionTime: time.Now().Add(300 * time.Millisecond)})
+	processor.Enqueue(&queueableItem{Name: "item4", ExecutionTime: time.Now().Add(time.Second)})
 
 	// Items with the same value returned by Key() are considered the same, so will be replaced
-	processor.Enqueue(&queueableItem{Name: "item3", ExecutionTime: clock.Now().Add(100 * time.Millisecond)})
+	processor.Enqueue(&queueableItem{Name: "item3", ExecutionTime: time.Now().Add(100 * time.Millisecond)})
 
 	// Using Dequeue allows removing an item from the queue
 	processor.Dequeue("item4")
