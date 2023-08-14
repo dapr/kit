569cc87ddb9bc281226f0a34eb475aed247af57f
Josh van Leeuwen <me@joshvanl.dev>
2023-08-14 21:04:38 +0100

    Adds batcher utility (#58)
    * Adds batcher utility
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Add `WithClock` for testing
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    * Renamees eventqueue to queue, and move queue and batcher under events
    
    Signed-off-by: joshvanl <me@joshvanl.dev>
    
    ---------
    
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- events/queue/eventqueue.go
+++ events/queue/eventqueue.go
@@ -15,4 +15,4 @@ limitations under the License.
 // Events are maintained in an in-memory queue, where items are in the order of when they are to be executed.
 // Users should interact with the Processor to process events in the queue.
 // When the queue has at least 1 item, the processor uses a single background goroutine to wait on the next item to be executed.
-package eventqueue
+package queue
