8b62372c7275e54571dffce2713365703f6a8f4b
joshvanl <me@joshvanl.dev>
2023-09-07 11:14:57 -0500

    Use correct package name in queue/processor_unit.go
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- events/queue/eventqueue.go
+++ events/queue/eventqueue.go
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package eventqueue implements a queue processor for delayed events.
+// Package queue implements a queue processor for delayed events.
 // Events are maintained in an in-memory queue, where items are in the order of when they are to be executed.
 // Users should interact with the Processor to process events in the queue.
 // When the queue has at least 1 item, the processor uses a single background goroutine to wait on the next item to be executed.
