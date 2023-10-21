abc80cf047d660b6ea2159160d7eb0474864d4d0
joshvanl <me@joshvanl.dev>
2023-10-21 11:05:34 +0100

    Adds events/ratelimiting/coalecing
    events/ratelimiting is a new utility package to provides helpers which
    can be used to rate limit events.
    
    Coalecing is a new helper that will exponentially rate limit events. It
    will coalesce events into a single event if they occur within the same
    rate limiting window. Coalesce also has the option to forcibly fire an
    event when the number of events reaches a certain threshold. Added to
    prevent events from never being fired in a high throughput scenario.
    
    Signed-off-by: joshvanl <me@joshvanl.dev>

---
+++ events/ratelimiting/coalescing.go
@@ -0,0 +1,258 @@
+/*
+Copyright 2023 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ratelimiting
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"k8s.io/utils/clock"
+)
+
+// OptionsCoalescing configures a Coalescing RateLimiter.
+type OptionsCoalescing struct {
+	// InitialDelay is the initial delay for the rate limiter. The rate limiter
+	// will not delay events less than the initial delay.
+	// Defaults to 500ms.
+	InitialDelay *time.Duration
+
+	// MaxDelay is the maximum delay for the rate limiter. The rate limiter will
+	// not delay events longer than the max delay.
+	// Defaults to 5s.
+	MaxDelay *time.Duration
+
+	// MaxPendingEvents is the maximum number of events that can pending on a
+	// rate limiter, before it fires an event anyway. Useful to prevent a rate
+	// limiter never firing events in a high throughput scenario.
+	// Defaults to unlimited.
+	MaxPendingEvents *int
+}
+
+// coalescing is a rate limiter that rate limits events. It coalesces events
+// that occur within a rate limiting window.
+type coalescing struct {
+	initialDelay     time.Duration
+	maxDelay         time.Duration
+	maxPendingEvents *int
+
+	pendingEvents int
+	timer         clock.Timer
+	hasTimer      atomic.Bool
+	inputCh       chan struct{}
+	currentDur    time.Duration
+	backoffFactor int
+
+	wg      sync.WaitGroup
+	lock    sync.RWMutex
+	clock   clock.WithTicker
+	running atomic.Bool
+	closeCh chan struct{}
+	closed  atomic.Bool
+}
+
+func NewCoalescing(opts OptionsCoalescing) (*coalescing, error) {
+	initialDelay := time.Millisecond * 500
+	if opts.InitialDelay != nil {
+		initialDelay = *opts.InitialDelay
+	}
+	if initialDelay < 0 {
+		return nil, errors.New("initial delay must be > 0")
+	}
+
+	maxDelay := time.Second * 5
+	if opts.MaxDelay != nil {
+		maxDelay = *opts.MaxDelay
+	}
+	if maxDelay < 0 {
+		return nil, errors.New("max delay must be > 0")
+	}
+
+	if maxDelay < initialDelay {
+		return nil, errors.New("max delay must be >= base delay")
+	}
+
+	if opts.MaxPendingEvents != nil && *opts.MaxPendingEvents <= 0 {
+		return nil, errors.New("max pending events must be > 0")
+	}
+
+	return &coalescing{
+		initialDelay:     initialDelay,
+		maxDelay:         maxDelay,
+		maxPendingEvents: opts.MaxPendingEvents,
+		currentDur:       initialDelay,
+		backoffFactor:    1,
+		inputCh:          make(chan struct{}),
+		closeCh:          make(chan struct{}),
+		clock:            clock.RealClock{},
+	}, nil
+}
+
+// Run runs the rate limiter. It will begin rate limiting events after the
+// first event is received.
+func (c *coalescing) Run(ctx context.Context, ch chan<- struct{}) error {
+	if !c.running.CompareAndSwap(false, true) {
+		return errors.New("already running")
+	}
+
+	// Prevent wg race condition on Close and Run.
+	c.lock.Lock()
+	c.wg.Add(1)
+	c.lock.Unlock()
+	defer c.wg.Done()
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	for {
+		// If the timer doesn't exist yet, we're waiting for the first event (which
+		// will fire immediately when received).
+		if !c.hasTimer.Load() {
+			select {
+			case <-ctx.Done():
+				return nil
+			case <-c.closeCh:
+				cancel()
+				return nil
+
+			case <-c.inputCh:
+				c.handleInputCh(ctx, ch)
+			}
+		} else {
+			// We already have a timer running, so we're waiting for either the timer
+			// to fire, or for a new event to arrive.
+			select {
+			case <-ctx.Done():
+				return nil
+			case <-c.closeCh:
+				cancel()
+				return nil
+
+			case <-c.inputCh:
+				c.handleInputCh(ctx, ch)
+			case <-c.timer.C():
+				c.handleTimerFired(ctx, ch)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (c *coalescing) handleInputCh(ctx context.Context, ch chan<- struct{}) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	switch {
+	case !c.hasTimer.Load():
+		// We don't have a timer yet, so this is the first event that has fired. We
+		// fire the event immediately, and set the timer to fire again after the
+		// initial delay.
+		c.timer = c.clock.NewTimer(c.initialDelay)
+		c.hasTimer.Store(true)
+		c.fireEvent(ctx, ch)
+
+	default:
+		// If maxPendingEvents is set and we have reached it then fire the event
+		// immediately.
+		if c.maxPendingEvents != nil && c.pendingEvents >= *c.maxPendingEvents {
+			c.fireEvent(ctx, ch)
+			return
+		}
+
+		if !c.timer.Stop() {
+			<-c.timer.C()
+		}
+
+		// Setup backoff. Backoff is exponential. If initial is 500ms and max is
+		// 5s, the backoff will follow:
+		// 500ms, 1s, 2s, 4s, 5s, 5s, 5s, ...
+		if c.currentDur < c.maxDelay {
+			c.backoffFactor *= 2
+			c.currentDur = time.Duration(float64(c.initialDelay) * float64(c.backoffFactor))
+			if c.currentDur > c.maxDelay {
+				c.currentDur = c.maxDelay
+			}
+		}
+
+		c.timer.Reset(c.currentDur)
+	}
+}
+
+func (c *coalescing) handleTimerFired(ctx context.Context, ch chan<- struct{}) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.fireEvent(ctx, ch)
+	c.reset()
+}
+
+func (c *coalescing) fireEvent(ctx context.Context, ch chan<- struct{}) {
+	// Important to only send on the channel if there are pending events,
+	// otherwise we will double send an event, for example if only a single event
+	// was sent and then the rate limiting window expired with no new events.
+	if c.pendingEvents > 0 {
+		c.pendingEvents = 0
+		c.wg.Add(1)
+		go func() {
+			defer c.wg.Done()
+			select {
+			case ch <- struct{}{}:
+			case <-ctx.Done():
+			}
+		}()
+	}
+}
+
+func (c *coalescing) reset() {
+	if !c.timer.Stop() {
+		select {
+		case <-c.timer.C():
+		default:
+		}
+	}
+
+	c.pendingEvents = 0
+	c.currentDur = c.initialDelay
+	c.backoffFactor = 1
+	c.hasTimer.Store(false)
+	c.timer = nil
+}
+
+func (c *coalescing) Add() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.pendingEvents++
+	c.wg.Add(1)
+	go func() {
+		defer c.wg.Done()
+		select {
+		case c.inputCh <- struct{}{}:
+		case <-c.closeCh:
+		}
+	}()
+}
+
+func (c *coalescing) Close() {
+	defer func() {
+		// Prevent wg race condition on Close and Run.
+		c.lock.Lock()
+		c.wg.Wait()
+		c.lock.Unlock()
+	}()
+	if c.closed.CompareAndSwap(false, true) {
+		close(c.closeCh)
+	}
+}
---
+++ events/ratelimiting/coalescing_test.go
@@ -0,0 +1,357 @@
+/*
+Copyright 2023 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ratelimiting
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/dapr/kit/ptr"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"k8s.io/utils/clock"
+	clocktesting "k8s.io/utils/clock/testing"
+)
+
+func TestCoalescing(t *testing.T) {
+	runCoalescingTests := func(t *testing.T, clock clock.WithTicker, opts OptionsCoalescing) (*coalescing, chan struct{}) {
+		t.Helper()
+		c, err := NewCoalescing(opts)
+		require.NoError(t, err)
+
+		if clock != nil {
+			c.WithTicker(clock)
+		}
+
+		ch := make(chan struct{})
+		errCh := make(chan error)
+		go func() {
+			errCh <- c.Run(context.Background(), ch)
+		}()
+
+		t.Cleanup(func() {
+			c.Close()
+
+			select {
+			case err := <-errCh:
+				assert.NoError(t, err)
+			case <-time.After(time.Second):
+				assert.Fail(t, "timeout")
+			}
+		})
+
+		return c, ch
+	}
+
+	assertChannel := func(t *testing.T, ch chan struct{}) {
+		t.Helper()
+		select {
+		case <-ch:
+		case <-time.After(time.Second):
+			assert.Fail(t, "timeout")
+		}
+	}
+
+	assertNoChannel := func(t *testing.T, ch chan struct{}) {
+		t.Helper()
+		select {
+		case <-ch:
+			assert.Fail(t, "should not have received event")
+		case <-time.After(time.Millisecond * 10):
+		}
+	}
+
+	t.Run("closing context should return Run", func(t *testing.T) {
+		c, err := NewCoalescing(OptionsCoalescing{})
+		assert.NoError(t, err)
+
+		ctx, cancel := context.WithCancel(context.Background())
+		errCh := make(chan error)
+		go func() {
+			errCh <- c.Run(ctx, make(chan struct{}))
+		}()
+
+		cancel()
+
+		select {
+		case err := <-errCh:
+			assert.NoError(t, err)
+		case <-time.After(time.Second):
+			assert.Fail(t, "timeout")
+		}
+	})
+
+	t.Run("calling Close should return Run", func(t *testing.T) {
+		c, err := NewCoalescing(OptionsCoalescing{})
+		assert.NoError(t, err)
+
+		errCh := make(chan error)
+		go func() {
+			errCh <- c.Run(context.Background(), make(chan struct{}))
+		}()
+
+		c.Close()
+
+		select {
+		case err := <-errCh:
+			assert.NoError(t, err)
+		case <-time.After(time.Second):
+			assert.Fail(t, "timeout")
+		}
+	})
+
+	t.Run("calling Run twice should error", func(t *testing.T) {
+		c, err := NewCoalescing(OptionsCoalescing{})
+		assert.NoError(t, err)
+
+		errCh := make(chan error)
+		go func() {
+			errCh <- c.Run(context.Background(), make(chan struct{}))
+		}()
+
+		c.Close()
+
+		select {
+		case err := <-errCh:
+			assert.NoError(t, err)
+		case <-time.After(time.Second):
+			assert.Fail(t, "timeout")
+		}
+
+		go func() {
+			errCh <- c.Run(context.Background(), make(chan struct{}))
+		}()
+
+		select {
+		case err := <-errCh:
+			assert.Error(t, err)
+		case <-time.After(time.Second):
+			assert.Fail(t, "timeout")
+		}
+	})
+
+	t.Run("options", func(t *testing.T) {
+		_, err := NewCoalescing(OptionsCoalescing{
+			InitialDelay: ptr.Of(-time.Second),
+		})
+		assert.Error(t, err)
+
+		_, err = NewCoalescing(OptionsCoalescing{
+			MaxDelay: ptr.Of(-time.Second),
+		})
+		assert.Error(t, err)
+
+		_, err = NewCoalescing(OptionsCoalescing{
+			MaxPendingEvents: ptr.Of(0),
+		})
+		assert.Error(t, err)
+		_, err = NewCoalescing(OptionsCoalescing{
+			MaxPendingEvents: ptr.Of(-1),
+		})
+		assert.Error(t, err)
+
+		_, err = NewCoalescing(OptionsCoalescing{
+			InitialDelay: ptr.Of(time.Second),
+			MaxDelay:     ptr.Of(time.Second / 2),
+		})
+		assert.Error(t, err)
+
+		_, err = NewCoalescing(OptionsCoalescing{
+			InitialDelay:     ptr.Of(time.Second),
+			MaxDelay:         ptr.Of(time.Second * 2),
+			MaxPendingEvents: ptr.Of(2),
+		})
+		assert.NoError(t, err)
+	})
+
+	t.Run("sending a single event initially should immediately send it", func(t *testing.T) {
+		c, ch := runCoalescingTests(t, nil, OptionsCoalescing{})
+		c.Add()
+
+		select {
+		case <-ch:
+		case <-time.After(time.Second):
+			assert.Fail(t, "timeout")
+		}
+	})
+
+	t.Run("second event after initial delay should not be rate limited", func(t *testing.T) {
+		clock := clocktesting.NewFakeClock(time.Now())
+		c, ch := runCoalescingTests(t, clock, OptionsCoalescing{
+			InitialDelay: ptr.Of(time.Second),
+			MaxDelay:     ptr.Of(time.Second * 2),
+		})
+
+		c.Add()
+		assertChannel(t, ch)
+
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second)
+
+		c.Add()
+		assertChannel(t, ch)
+		assertNoChannel(t, ch)
+	})
+
+	t.Run("second event before initial delay should be rate limited", func(t *testing.T) {
+		clock := clocktesting.NewFakeClock(time.Now())
+		c, ch := runCoalescingTests(t, clock, OptionsCoalescing{
+			InitialDelay: ptr.Of(time.Second),
+			MaxDelay:     ptr.Of(time.Second * 2),
+		})
+
+		c.Add()
+		assertChannel(t, ch)
+
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second / 2)
+
+		c.Add()
+		assertNoChannel(t, ch)
+
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second * 2)
+		assertChannel(t, ch)
+		assertNoChannel(t, ch)
+	})
+
+	t.Run("multiple events before initial delay should be rate limited to single event", func(t *testing.T) {
+		clock := clocktesting.NewFakeClock(time.Now())
+		c, ch := runCoalescingTests(t, clock, OptionsCoalescing{
+			InitialDelay: ptr.Of(time.Second),
+			MaxDelay:     ptr.Of(time.Second * 2),
+		})
+
+		c.Add()
+		assertChannel(t, ch)
+
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second / 2)
+
+		c.Add()
+		c.Add()
+		c.Add()
+		assertNoChannel(t, ch)
+
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second * 2)
+		assertChannel(t, ch)
+		assertNoChannel(t, ch)
+	})
+
+	t.Run("rate limiting should increase if events keep being added", func(t *testing.T) {
+		clock := clocktesting.NewFakeClock(time.Now())
+		c, ch := runCoalescingTests(t, clock, OptionsCoalescing{
+			InitialDelay: ptr.Of(time.Second),
+			MaxDelay:     ptr.Of(time.Second * 5),
+		})
+
+		c.Add()
+		assertChannel(t, ch)
+
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second / 2)
+		c.Add()
+		assertNoChannel(t, ch)
+
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second * 1)
+		c.Add()
+		assertNoChannel(t, ch)
+
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second * 2)
+		c.Add()
+		assertNoChannel(t, ch)
+
+		for i := 0; i < 4; i++ {
+			assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+			clock.Step(time.Second * 4)
+			c.Add()
+			assertNoChannel(t, ch)
+		}
+
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second * 5)
+		assertChannel(t, ch)
+		assertNoChannel(t, ch)
+	})
+
+	t.Run("should fire event if reached maximum pending events", func(t *testing.T) {
+		clock := clocktesting.NewFakeClock(time.Now())
+		c, ch := runCoalescingTests(t, clock, OptionsCoalescing{
+			InitialDelay:     ptr.Of(time.Second),
+			MaxDelay:         ptr.Of(time.Second * 5),
+			MaxPendingEvents: ptr.Of(3),
+		})
+
+		c.Add()
+		assertChannel(t, ch)
+
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second / 2)
+		c.Add()
+		assertNoChannel(t, ch)
+
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second * 1)
+		c.Add()
+		assertNoChannel(t, ch)
+
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second * 1)
+		// We have reached 3 pending events so should fire event though we are rate
+		// limited.
+		c.Add()
+		assertChannel(t, ch)
+
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second * 2)
+		c.Add()
+		assertNoChannel(t, ch)
+
+		// Expire rate limit and fire event.
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second * 5)
+		assertChannel(t, ch)
+		assertNoChannel(t, ch)
+
+		// New event should fire immediately.
+		c.Add()
+		assertChannel(t, ch)
+	})
+
+	t.Run("lots of events fired in the first rate limiting window will trigger 2 event ommited", func(t *testing.T) {
+		clock := clocktesting.NewFakeClock(time.Now())
+		c, ch := runCoalescingTests(t, clock, OptionsCoalescing{
+			InitialDelay: ptr.Of(time.Second),
+			MaxDelay:     ptr.Of(time.Second * 5),
+		})
+
+		c.Add()
+		assertChannel(t, ch)
+
+		assert.Eventually(t, c.hasTimer.Load, time.Second, time.Millisecond)
+		for i := 0; i < 10; i++ {
+			c.Add()
+		}
+		assert.Eventually(t, clock.HasWaiters, time.Second, time.Millisecond)
+		clock.Step(time.Second * 5)
+
+		assertChannel(t, ch)
+		assert.False(t, clock.HasWaiters())
+		assertNoChannel(t, ch)
+	})
+}
