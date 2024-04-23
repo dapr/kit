e7b67515520110eae4f435b8106cedc18fb1e092
Elena Kolevska <elena@kolevska.com>
2024-04-24 00:02:14 +0100

    More work on atomic map and mutex map
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- concurrency/atomicmap.go
+++ concurrency/atomicmap.go
@@ -6,142 +6,150 @@ import (
 )
 
 type AtomicMap interface {
-	Init(key string)
 	Delete(key string)
 	Get(key string) interface{}
+	OuterLock()
+	OuterUnlock()
+	OuterRLock()
+	OuterRUnlock()
 }
 
+type AtomicMapBase struct{
+	mu sync.RWMutex
+}
+
+func(mm *AtomicMapBase) OuterLock(){
+	mm.mu.Lock()
+}
+
+func(mm *AtomicMapBase) OuterUnlock(){
+	mm.mu.Unlock()
+}
+
+func(mm *AtomicMapBase) OuterRLock(){
+	mm.mu.RLock()
+}
+
+func(mm *AtomicMapBase) OuterRUnlock(){
+	mm.mu.RUnlock()
+}
+
+
 type AtomicMapInt32 struct {
-	mu    sync.RWMutex
-	items map[string]atomic.Int32
+	AtomicMapBase
+	Items map[string]*atomic.Int32
 }
 
 func NewAtomicMapInt32() *AtomicMapInt32 {
 	return &AtomicMapInt32{
-		items: make(map[string]atomic.Int32),
+		Items: make(map[string]*atomic.Int32),
 	}
 }
 
-func (m *AtomicMapInt32) Init(key string) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
+func (m *AtomicMapInt32) Get(key string) *atomic.Int32 {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
 
-	m.items[key] = atomic.Int32{}
+	if _, ok := m.Items[key]; !ok {
+		m.Items[key] = &atomic.Int32{}
+	}
+
+
+	return m.Items[key]
 }
 
 func (m *AtomicMapInt32) Delete(key string) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
-	delete(m.items, key)
-}
-
-func (m *AtomicMapInt32) Get(key string) atomic.Int32 {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
-
-	return m.items[key]
+	delete(m.Items, key)
 }
 
 type AtomicMapUint32 struct {
-	mu    sync.RWMutex
-	items map[string]atomic.Uint32
+	AtomicMapBase
+	Items map[string]*atomic.Uint32
 }
 
 func NewAtomicMapUint32() *AtomicMapUint32 {
 	return &AtomicMapUint32{
-		items: make(map[string]atomic.Uint32),
+		Items: make(map[string]*atomic.Uint32),
 	}
 }
 
-func (m *AtomicMapUint32) Init(key string) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
+func (m *AtomicMapUint32) Get(key string) *atomic.Uint32 {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
 
-	m.items[key] = atomic.Uint32{}
+	if _, ok := m.Items[key]; !ok {
+		m.Items[key] = &atomic.Uint32{}
+	}
+
+	return m.Items[key]
 }
 
 func (m *AtomicMapUint32) Delete(key string) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
-	delete(m.items, key)
+	delete(m.Items, key)
 }
 
-func (m *AtomicMapUint32) Get(key string) atomic.Uint32 {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
-
-	return m.items[key]
-}
 
 type AtomicMapInt64 struct {
-	mu    sync.RWMutex
-	items map[string]atomic.Int64
+	AtomicMapBase
+	Items map[string]*atomic.Int64
 }
 
-// NewAtomicMapInt64 creates a new AtomicMapInt64 instance.
 func NewAtomicMapInt64() *AtomicMapInt64 {
 	return &AtomicMapInt64{
-		items: make(map[string]atomic.Int64),
+		Items: make(map[string]*atomic.Int64),
 	}
 }
 
-// Add adds a new int64 value to the map with the given key.
-func (m *AtomicMapInt64) Init(key string) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
+func (m *AtomicMapInt64) Get(key string) *atomic.Int64 {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
 
-	m.items[key] = atomic.Int64{}
+	if _, ok := m.Items[key]; !ok {
+		m.Items[key] = &atomic.Int64{}
+	}
+
+
+	return m.Items[key]
 }
 
-// Delete deletes the int64 value with the given key from the map.
 func (m *AtomicMapInt64) Delete(key string) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
-	delete(m.items, key)
-}
-
-// Get retrieves the int64 value associated with the given key from the map.
-func (m *AtomicMapInt64) Get(key string) atomic.Int64 {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
-
-	return m.items[key]
+	delete(m.Items, key)
 }
 
 type AtomicMapUint64 struct {
-	mu    sync.RWMutex
-	items map[string]atomic.Uint64
+	AtomicMapBase
+	Items map[string]*atomic.Uint64
 }
 
 func NewAtomicMapUint64() *AtomicMapUint64 {
 	return &AtomicMapUint64{
-		items: make(map[string]atomic.Uint64),
+		Items: make(map[string]*atomic.Uint64),
 	}
 }
 
-// Add adds a new int64 value to the map with the given key.
-func (m *AtomicMapUint64) Init(key string) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
+func (m *AtomicMapUint64) Get(key string) *atomic.Uint64 {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
 
-	m.items[key] = atomic.Uint64{}
+	if _, ok := m.Items[key]; !ok {
+		m.Items[key] = &atomic.Uint64{}
+	}
+
+	return m.Items[key]
 }
 
-// Delete deletes the int64 value with the given key from the map.
 func (m *AtomicMapUint64) Delete(key string) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
-	delete(m.items, key)
-}
-
-// Get retrieves the int64 value associated with the given key from the map.
-func (m *AtomicMapUint64) Get(key string) atomic.Uint64 {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
-
-	return m.items[key]
+	delete(m.Items, key)
 }
--- concurrency/mutexmap.go
+++ concurrency/mutexmap.go
@@ -4,12 +4,12 @@ import "sync"
 
 type MutexMap struct {
 	mu    sync.RWMutex // outer lock
-	mutex map[string]*sync.Mutex // inner locks
+	mutex map[string]*sync.RWMutex // inner locks
 }
 
 func NewMutexMap() *MutexMap {
 	return &MutexMap{
-		mutex: make(map[string]*sync.Mutex),
+		mutex: make(map[string]*sync.RWMutex),
 	}
 }
 
@@ -20,7 +20,7 @@ func (mm *MutexMap) Lock(key string) {
 		mm.mu.RUnlock()
 		mm.mu.Lock()
 		if mm.mutex[key] == nil {
-			mm.mutex[key] = &sync.Mutex{}
+			mm.mutex[key] = &sync.RWMutex{}
 		}
 		mm.mutex[key].Lock()
 		mm.mu.Unlock()
@@ -39,20 +39,67 @@ func (mm *MutexMap) Unlock(key string) {
 	}
 }
 
-func (mm *MutexMap) Add(key string) {
-	mm.mu.Lock()
-	defer mm.mu.Unlock()
+func (mm *MutexMap) RLock(key string) {
+	mm.mu.RLock()
 
 	if mm.mutex[key] == nil {
-		mm.mutex[key] = &sync.Mutex{}
+		mm.mu.RUnlock()
+		mm.mu.Lock()
+		if mm.mutex[key] == nil {
+			mm.mutex[key] = &sync.RWMutex{}
+		}
+		mm.mutex[key].RLock()
+		mm.mu.Unlock()
+		return
 	}
+	mm.mutex[key].RLock()
+	mm.mu.RUnlock()
 }
 
-func (mm *MutexMap) Delete(key string) {
+func (mm *MutexMap) RUnlock(key string) {
 	mm.mu.Lock()
 	defer mm.mu.Unlock()
 
 	if mm.mutex[key] != nil {
+		mm.mutex[key].RUnlock()
+	}
+}
+
+
+func (mm *MutexMap) Add(key string, lock bool) {
+	if lock {
+		mm.mu.Lock()
+		defer mm.mu.Unlock()
+	}
+
+	if mm.mutex[key] == nil {
+		mm.mutex[key] = &sync.RWMutex{}
+	}
+}
+
+func (mm *MutexMap) Delete(key string, lock bool) {
+	if lock {
+		mm.mu.Lock()
+		defer mm.mu.Unlock()
+	}
+
+	if mm.mutex[key] != nil {
 		delete(mm.mutex, key)
 	}
+}
+
+func(mm *MutexMap) OuterLock(){
+	mm.mu.Lock()
+}
+
+func(mm *MutexMap) OuterUnlock(){
+	mm.mu.Unlock()
+}
+
+func(mm *MutexMap) OuterRLock(){
+	mm.mu.RLock()
+}
+
+func(mm *MutexMap) OuterRUnlock(){
+	mm.mu.RUnlock()
 }
