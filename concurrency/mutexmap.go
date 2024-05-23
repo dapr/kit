106329e5839f70f3234b9f49b7dabff0247c0a90
Elena Kolevska <elena-kolevska@users.noreply.github.com>
2024-05-23 23:57:05 +0100

    Mutexmap (#95)
    * Adds Mutex Map
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * Adds an atomic map
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * More work on atomic map and mutex map
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * Fixes, improvements and more tests
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * Updates interface
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * Linter
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * Refactors atomic map to use generics
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * cleanups
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Cassie Coyle <cassie.i.coyle@gmail.com>
    Signed-off-by: Elena Kolevska <elena-kolevska@users.noreply.github.com>
    
    * small reorg
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * Adds ItemCount()
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * Removes atomicmap in favour of haxmap
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * formats fix and adds comment
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * Update concurrency/mutexmap.go
    
    Co-authored-by: Josh van Leeuwen <me@joshvanl.dev>
    Signed-off-by: Elena Kolevska <elena-kolevska@users.noreply.github.com>
    
    * Uses built in `clear`
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * Revert "Removes atomicmap in favour of haxmap"
    
    This reverts commit 20ca9ad19721361c95a42ec07c30797dd02f1239.
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * Uses clear() for atomic map too
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    ---------
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    Signed-off-by: Elena Kolevska <elena-kolevska@users.noreply.github.com>
    Co-authored-by: Cassie Coyle <cassie.i.coyle@gmail.com>
    Co-authored-by: Josh van Leeuwen <me@joshvanl.dev>

---
+++ concurrency/mutexmap.go
@@ -0,0 +1,119 @@
+/*
+Copyright 2024 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package concurrency
+
+import (
+	"sync"
+)
+
+// MutexMap is an interface that defines a thread-safe map with keys of type T associated to
+// read-write mutexes (sync.RWMutex), allowing for granular locking on a per-key basis.
+// This can be useful for scenarios where fine-grained concurrency control is needed.
+//
+// Methods:
+// - Lock(key T): Acquires an exclusive lock on the mutex associated with the given key.
+// - Unlock(key T): Releases the exclusive lock on the mutex associated with the given key.
+// - RLock(key T): Acquires a read lock on the mutex associated with the given key.
+// - RUnlock(key T): Releases the read lock on the mutex associated with the given key.
+// - Delete(key T): Removes the mutex associated with the given key from the map.
+// - Clear(): Removes all mutexes from the map.
+// - ItemCount() int: Returns the number of items (mutexes) in the map.
+type MutexMap[T comparable] interface {
+	Lock(key T)
+	Unlock(key T)
+	RLock(key T)
+	RUnlock(key T)
+	Delete(key T)
+	Clear()
+	ItemCount() int
+}
+
+type mutexMap[T comparable] struct {
+	lock  sync.RWMutex
+	items map[T]*sync.RWMutex
+}
+
+func NewMutexMap[T comparable]() MutexMap[T] {
+	return &mutexMap[T]{
+		items: make(map[T]*sync.RWMutex),
+	}
+}
+
+func (a *mutexMap[T]) Lock(key T) {
+	a.lock.RLock()
+	mutex, ok := a.items[key]
+	a.lock.RUnlock()
+	if !ok {
+		a.lock.Lock()
+		mutex, ok = a.items[key]
+		if !ok {
+			mutex = &sync.RWMutex{}
+			a.items[key] = mutex
+		}
+		a.lock.Unlock()
+	}
+	mutex.Lock()
+}
+
+func (a *mutexMap[T]) Unlock(key T) {
+	a.lock.RLock()
+	mutex, ok := a.items[key]
+	a.lock.RUnlock()
+	if ok {
+		mutex.Unlock()
+	}
+}
+
+func (a *mutexMap[T]) RLock(key T) {
+	a.lock.RLock()
+	mutex, ok := a.items[key]
+	a.lock.RUnlock()
+	if !ok {
+		a.lock.Lock()
+		mutex, ok = a.items[key]
+		if !ok {
+			mutex = &sync.RWMutex{}
+			a.items[key] = mutex
+		}
+		a.lock.Unlock()
+	}
+	mutex.Lock()
+}
+
+func (a *mutexMap[T]) RUnlock(key T) {
+	a.lock.RLock()
+	mutex, ok := a.items[key]
+	a.lock.RUnlock()
+	if ok {
+		mutex.Unlock()
+	}
+}
+
+func (a *mutexMap[T]) Delete(key T) {
+	a.lock.Lock()
+	delete(a.items, key)
+	a.lock.Unlock()
+}
+
+func (a *mutexMap[T]) Clear() {
+	a.lock.Lock()
+	clear(a.items)
+	a.lock.Unlock()
+}
+
+func (a *mutexMap[T]) ItemCount() int {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	return len(a.items)
+}
