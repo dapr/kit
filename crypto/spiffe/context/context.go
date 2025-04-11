64ff0e4d9777deed0bdebebecabac1a8cf527b96
Jonathan Collinge <jonathancollinge@live.com>
2025-04-11 08:53:07 +0100

    Add spiffecontext JWT methods
    Signed-off-by: Jonathan Collinge <jonathancollinge@live.com>

--- crypto/spiffe/context/context.go
+++ crypto/spiffe/context/context.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 The Dapr Authors
+Copyright 2025 The Dapr Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -16,6 +16,7 @@ package context
 import (
 	"context"
 
+	"github.com/spiffe/go-spiffe/v2/svid/jwtsvid"
 	"github.com/spiffe/go-spiffe/v2/svid/x509svid"
 
 	"github.com/dapr/kit/crypto/spiffe"
@@ -23,13 +24,42 @@ import (
 
 type ctxkey int
 
-const svidKey ctxkey = iota
+const (
+	x509SvidKey ctxkey = iota
+	jwtSvidKey
+)
 
+// Deprecated: use WithX509 instead.
+// With adds the x509 SVID source from the SPIFFE object to the context.
 func With(ctx context.Context, spiffe *spiffe.SPIFFE) context.Context {
-	return context.WithValue(ctx, svidKey, spiffe.SVIDSource())
+	return context.WithValue(ctx, x509SvidKey, spiffe.SVIDSource())
 }
 
+// Deprecated: use FromX509 instead.
+// From retrieves the x509 SVID source from the context.
 func From(ctx context.Context) (x509svid.Source, bool) {
-	svid, ok := ctx.Value(svidKey).(x509svid.Source)
+	svid, ok := ctx.Value(x509SvidKey).(x509svid.Source)
+	return svid, ok
+}
+
+// WithX509 adds an x509 SVID source to the context.
+func WithX509(ctx context.Context, source x509svid.Source) context.Context {
+	return context.WithValue(ctx, x509SvidKey, source)
+}
+
+// WithJWT adds a JWT SVID source to the context.
+func WithJWT(ctx context.Context, source jwtsvid.Source) context.Context {
+	return context.WithValue(ctx, jwtSvidKey, source)
+}
+
+// FromX509 retrieves the x509 SVID source from the context.
+func FromX509(ctx context.Context) (x509svid.Source, bool) {
+	svid, ok := ctx.Value(x509SvidKey).(x509svid.Source)
+	return svid, ok
+}
+
+// FromJWT retrieves the JWT SVID source from the context.
+func FromJWT(ctx context.Context) (jwtsvid.Source, bool) {
+	svid, ok := ctx.Value(jwtSvidKey).(jwtsvid.Source)
 	return svid, ok
 }
