d72448361f2375c642840853bb46120b9e1451b1
Jonathan Collinge <jonathancollinge@live.com>
2025-04-11 09:54:32 +0100

    Adds JWT handling to spiffe
    Signed-off-by: Jonathan Collinge <jonathancollinge@live.com>

--- crypto/spiffe/svidsource.go
+++ crypto/spiffe/svidsource.go
@@ -14,27 +14,49 @@ limitations under the License.
 package spiffe
 
 import (
+	"context"
 	"errors"
 
+	"github.com/spiffe/go-spiffe/v2/svid/jwtsvid"
 	"github.com/spiffe/go-spiffe/v2/svid/x509svid"
 )
 
-// svidSource is an implementation of the Go spiffe x509svid Source interface.
+// svidSource is an implementation of both go-spiffe x509svid.Source and jwtsvid.Source interfaces.
 type svidSource struct {
 	spiffe *SPIFFE
 }
 
 // GetX509SVID returns the current X.509 certificate identity as a SPIFFE SVID.
-// Implements the go-spiffe x509 source interface.
+// Implements the go-spiffe x509svid.Source interface.
 func (s *svidSource) GetX509SVID() (*x509svid.SVID, error) {
 	s.spiffe.lock.RLock()
 	defer s.spiffe.lock.RUnlock()
 
 	<-s.spiffe.readyCh
 
-	svid := s.spiffe.currentSVID
+	svid := s.spiffe.currentX509SVID
 	if svid == nil {
-		return nil, errors.New("no SVID available")
+		return nil, errors.New("no X509 SVID available")
+	}
+
+	return svid, nil
+}
+
+// FetchJWTSVID returns the current JWT SVID.
+// Implements the go-spiffe jwtsvid.Source interface.
+func (s *svidSource) FetchJWTSVID(ctx context.Context, params jwtsvid.Params) (*jwtsvid.SVID, error) {
+	s.spiffe.lock.RLock()
+	defer s.spiffe.lock.RUnlock()
+
+	<-s.spiffe.readyCh
+
+	svid := s.spiffe.currentJWTSVID
+	if svid == nil {
+		return nil, errors.New("no JWT SVID available")
+	}
+
+	if !audiencesMatch(svid.Audience, []string{params.Audience}) {
+		return nil, errors.New("JWT SVID has different audiences than requested")
 	}
 
 	return svid, nil
